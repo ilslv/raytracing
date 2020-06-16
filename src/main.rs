f19684dceef85faf51a05b58180856d75b02f306
ilslv <ilya.solovyiov@gmail.com>
2020-06-16 19:37:22 +0300

    Sampling implemented

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod ray;
 mod hit;
 mod sphere;
 mod hittable_list;
+mod camera;
 
 use crate::color::Color;
 use crate::ray::Ray;
@@ -14,6 +15,8 @@ use crate::hit::{Hit, HitRecord};
 use crate::hittable_list::HittableList;
 use std::rc::Rc;
 use crate::sphere::Sphere;
+use crate::camera::Camera;
+use rand::Rng;
 
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const IMAGE_WIDTH: u32 = 384;
@@ -35,14 +38,11 @@ fn ray_color(ray: &Ray, world: &impl Hit) -> Color {
 fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
+    let samples_per_pixel = 100;
 
-    let origin = Point3::new(0., 0., 0.);
-    let horizontal = Vec3::new(viewport_width, 0., 0.);
-    let vertical = Vec3::new(0., viewport_height, 0.);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0., 0., focal_length);
+    let mut rng = rand::thread_rng();
+
+    let cam = Camera::new();
 
     let mut world = HittableList::default();
     world.add(Rc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
@@ -51,17 +51,15 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Lines remained {}", j);
         for i in 0..IMAGE_WIDTH {
-            let u = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let v = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
+            for s in 0..samples_per_pixel {
+                let u = (i as f32 + rng.gen::<f32>()) / (IMAGE_WIDTH - 1) as f32;
+                let v = (j as f32 + rng.gen::<f32>()) / (IMAGE_HEIGHT - 1) as f32;
+                let ray = cam.get_ray(u, v);
+                pixel_color += Vec3::from(ray_color(&ray, &world)) / samples_per_pixel as f32;
+            }
 
-            let r = Ray::new(
-                origin,
-                (lower_left_corner + u * horizontal + v * vertical - origin.into()).into(),
-            );
-
-            let pixel_color = ray_color(&r, &world);
-
-            print!("{}\n", pixel_color);
+            print!("{}\n", Color::from(pixel_color));
         }
     }
     eprintln!("Done!");
